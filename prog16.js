285d7e5c10d5aca193b0e2901d753c48d290288e
P Rohit <40255346+prohit304@users.noreply.github.com>
2018-07-27 11:56:21 +0530

    First javaScript File

---
+++ prog16.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+function getValue() {
+   var retVal=prompt("Enter Your Name :","Your Name Here");
+    document.write("You have Entered : "+retVal);
+}
+  
+</script>
+ 
